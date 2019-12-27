caa175275567c476840a9dd4bf42cbe748ef85b5
nadanr <32873404+nadanr@users.noreply.github.com>
2019-12-27 20:13:16 +0700

    Initial

---
+++ Assets/Script/ColorPicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    public Material[] BodyColorMat;
+    Material CurrMat;
+    Renderer renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        renderer = this.GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //render blue color
+    public void ChangeColor(Material nextMaterial)
+    {
+        renderer.material = nextMaterial;
+        CurrMat = renderer.material;
+    }
+
+}
